a973627fec328c5dc35943120b7db6c3220a308a
amm0116 <amm-personal@outlook.com>
2023-12-03 18:04:33 -0500

    Add meta keywords

--- waywize/pages/_app.js
+++ waywize/pages/_app.js
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }) {
     <Head>
       <title>WayWize Mapping & Wayfinding Solutions</title>
       <meta name="description" content="Discover seamless outdoor and indoor navigation with our mapping and wayfinding solutions. Our software offers effortless navigational precision and a user-friendly layout." />
+      <meta name="keywords" content="customized mapping, customized wayfinding, navigation app, indoor navigation, AI mapping, event venue mapping, indoor mapping software, custom business mapping" />
     </Head>
     <ChakraProvider>
     <Component {...pageProps} />
