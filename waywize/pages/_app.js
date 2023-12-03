d4964761289dbaac6349eee5ac8f2966fb8b43d3
amm0116 <amm-personal@outlook.com>
2023-12-03 18:02:59 -0500

    Add app-wide meta description

--- waywize/pages/_app.js
+++ waywize/pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
     <>
     <Head>
       <title>WayWize Mapping & Wayfinding Solutions</title>
+      <meta name="description" content="Discover seamless outdoor and indoor navigation with our mapping and wayfinding solutions. Our software offers effortless navigational precision and a user-friendly layout." />
     </Head>
     <ChakraProvider>
     <Component {...pageProps} />
