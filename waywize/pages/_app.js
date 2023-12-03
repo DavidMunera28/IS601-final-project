09196bebbb549c5523c7b71ca0d79ab644c014f0
amm0116 <amm-personal@outlook.com>
2023-12-03 18:07:41 -0500

    Add app-wide favicon

--- waywize/pages/_app.js
+++ waywize/pages/_app.js
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }) {
       <meta name="description" content="Discover seamless outdoor and indoor navigation with our mapping and wayfinding solutions. Our software offers effortless navigational precision and a user-friendly layout." />
       <meta name="keywords" content="customized mapping, customized wayfinding, navigation app, indoor navigation, AI mapping, event venue mapping, indoor mapping software, custom business mapping" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
     <ChakraProvider>
     <Component {...pageProps} />
