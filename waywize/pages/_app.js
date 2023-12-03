e35df270a2d2cc26aa0dd689bac5bd5b03fc5fbc
amm0116 <amm-personal@outlook.com>
2023-12-03 17:59:10 -0500

    Add app-wide title

--- waywize/pages/_app.js
+++ waywize/pages/_app.js
@@ -1,9 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>WayWize Mapping & Wayfinding Solutions</title>
+    </Head>
     <ChakraProvider>
     <Component {...pageProps} />
     </ChakraProvider>
+    </>
   )
 }
