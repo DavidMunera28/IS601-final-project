3c8b1c17c91115410a6ed9f43deef953149b58f4
amm0116 <amm-personal@outlook.com>
2023-12-03 20:54:45 -0500

    Move navbar component into ChakraProvider

--- waywize/pages/_app.js
+++ waywize/pages/_app.js
@@ -12,8 +12,8 @@ export default function App({ Component, pageProps }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Navbar />
     <ChakraProvider>
+    <Navbar />
     <Component {...pageProps} />
     </ChakraProvider>
     </>
