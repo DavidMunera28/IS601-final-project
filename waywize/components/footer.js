46bdb6cf8e7633be0b51ab90f27b67ad3964f808
amm0116 <amm-personal@outlook.com>
2023-12-03 21:02:33 -0500

    Add list of internal navigation pages

--- waywize/components/footer.js
+++ waywize/components/footer.js
@@ -1,7 +1,14 @@
+import { List, ListItem } from "@chakra-ui/react";
+
 export default function Footer() {
     return (
       <footer>
-        
+        <List className="internal-links">
+            <ListItem>Home</ListItem>
+            <ListItem>About</ListItem>
+            <ListItem>Subscribe</ListItem>
+            <ListItem>Privacy Policy</ListItem>
+        </List>
       </footer>
     )
   }
