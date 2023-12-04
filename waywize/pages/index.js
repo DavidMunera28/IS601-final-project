d84099a36e9da27e6466bb85b015516627af36d7
amm0116 <amm-personal@outlook.com>
2023-12-03 20:18:38 -0500

    Add placeholder text to index

--- waywize/pages/index.js
+++ waywize/pages/index.js
@@ -1,7 +1,10 @@
+import { Heading, Text } from "@chakra-ui/react";
+
 export default function Home() {
   return (
     <>
-      
+      <Heading>Landing Page</Heading>
+      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper auctor neque vitae tempus quam pellentesque nec nam. Mattis aliquam faucibus purus in massa tempor. Tortor consequat id porta nibh venenatis cras sed felis. Nunc lobortis mattis aliquam faucibus purus in massa tempor nec. Pulvinar mattis nunc sed blandit. Venenatis cras sed felis eget. Elementum pulvinar etiam non quam lacus suspendisse. Mi eget mauris pharetra et ultrices neque ornare. Risus in hendrerit gravida rutrum quisque. Dictum sit amet justo donec enim diam. Sed risus ultricies tristique nulla aliquet enim tortor. Ultrices dui sapien eget mi.</Text>
     </>
   )
 }
