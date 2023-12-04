282363e57e8f6d377cd16e2f813bf98bb3c9f8a8
amm0116 <amm-personal@outlook.com>
2023-12-03 20:26:18 -0500

    Add placeholder text to about

--- waywize/pages/about.js
+++ waywize/pages/about.js
@@ -1,7 +1,10 @@
+import { Heading, Text } from "@chakra-ui/react";
+
 export default function About() {
     return (
       <>
-        
+        <Heading>About Page</Heading>
+        <Text>A erat nam at lectus urna. Praesent elementum facilisis leo vel fringilla. Commodo viverra maecenas accumsan lacus. Morbi tristique senectus et netus et malesuada fames ac turpis. Mauris vitae ultricies leo integer malesuada nunc vel risus. Purus sit amet volutpat consequat mauris nunc congue nisi. Pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus sit. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Pharetra pharetra massa massa ultricies mi. Lobortis scelerisque fermentum dui faucibus in ornare. Congue mauris rhoncus aenean vel elit scelerisque.</Text>
       </>
     )
   }
