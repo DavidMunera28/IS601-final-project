4d93bd5f34b080652dd9ccaaeca986e825727b68
amm0116 <amm-personal@outlook.com>
2023-12-03 20:51:29 -0500

    Add Home link

--- waywize/components/navbar.js
+++ waywize/components/navbar.js
@@ -1,10 +1,11 @@
 import { List, ListItem } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
       <nav>
         <List>
-            <ListItem>Home</ListItem>
+            <ListItem><Link href="/">Home</Link></ListItem>
             <ListItem>About</ListItem>
             <ListItem>Subscribe</ListItem>
         </List>
