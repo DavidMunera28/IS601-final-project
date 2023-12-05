5e518b4496069089bc90d23adaf065102b811e5f
amm0116 <amm-personal@outlook.com>
2023-12-04 19:28:35 -0500

    Import subscribe component into index page

--- waywize/pages/index.js
+++ waywize/pages/index.js
@@ -1,3 +1,4 @@
+import Subscribe from "@/components/subscribe";
 import { Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <Heading>Landing Page</Heading>
       <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper auctor neque vitae tempus quam pellentesque nec nam. Mattis aliquam faucibus purus in massa tempor. Tortor consequat id porta nibh venenatis cras sed felis. Nunc lobortis mattis aliquam faucibus purus in massa tempor nec. Pulvinar mattis nunc sed blandit. Venenatis cras sed felis eget. Elementum pulvinar etiam non quam lacus suspendisse. Mi eget mauris pharetra et ultrices neque ornare. Risus in hendrerit gravida rutrum quisque. Dictum sit amet justo donec enim diam. Sed risus ultricies tristique nulla aliquet enim tortor. Ultrices dui sapien eget mi.</Text>
+      <Subscribe />
     </>
   )
 }
