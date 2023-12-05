71aa5aa9c0e2429858e4edf699da96e558851e82
amm0116 <amm-personal@outlook.com>
2023-12-05 18:02:49 -0500

    Add GA_MEASUREMENT_ID prop

--- waywize/components/GoogleAnalytics.js
+++ waywize/components/GoogleAnalytics.js
@@ -2,7 +2,7 @@
 
 import Script from "next/script";
 
-export default function GoogleAnalytics() {
+export default function GoogleAnalytics({GA_MEASUREMENT_ID}) {
     return (
         <>
             <Script strategy="afterInteractive" 
