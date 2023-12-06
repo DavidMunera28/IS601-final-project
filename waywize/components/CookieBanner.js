f39bf5bbc7332fd39367ec3a73fcd852f8caf73e
amm0116 <amm-personal@outlook.com>
2023-12-05 19:17:39 -0500

    Add link to 'privacy policy' text

--- waywize/components/CookieBanner.js
+++ waywize/components/CookieBanner.js
@@ -1,11 +1,12 @@
 import { Button, Card, Text } from "@chakra-ui/react"
+import Link from "next/link"
 
 export default function CookieBanner(){
 
 return (
     <Card id="cookie-banner">
         <div class="cookie-text">
-            <Text>This site uses cookies to enhance the user experience. By clicking 'Accept All', you consent to the use of all cookies. By clicking 'Essential Only', you consent only to the use of essential cookies. You can review our Privacy Policy for more information about what data we collect.
+            <Text>This site uses cookies to enhance the user experience. By clicking 'Accept All', you consent to the use of all cookies. By clicking 'Essential Only', you consent only to the use of essential cookies. You can review our <Link href="privacy">Privacy Policy</Link> for more information about what data we collect.
             </Text>
         </div>
 
