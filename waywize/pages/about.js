3936fec9f1e2e00efe3d2a5afb09f5ba03e257d2
amm0116 <amm-personal@outlook.com>
2023-12-07 15:41:07 -0500

    Import what-we-stand-for-text.md into about page

--- waywize/pages/about.js
+++ waywize/pages/about.js
@@ -5,22 +5,26 @@ import { Heading } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), '/editable-content/overview-text.md');
-  const markdownContent = fs.readFileSync(filePath, 'utf-8');
+  const filePathOverview = path.join(process.cwd(), '/editable-content/overview-text.md');
+  const markdownContentOverview = fs.readFileSync(filePathOverview, 'utf-8');
+  const filePathWwsf = path.join(process.cwd(), '/editable-content/what-we-stand-for-text.md');
+  const markdownContentWwsf = fs.readFileSync(filePathWwsf, 'utf-8');
 
   return {
     props: {
-      markdownContent,
+      markdownContentOverview,
+      markdownContentWwsf,
     },
   };
 }
 
-export default function About({ markdownContent }) {
+export default function About({ markdownContentOverview, markdownContentWwsf }) {
     return (
       <>
         <Heading as="h1">About Page</Heading>
         <Image src="/images/OVERVIEW.png" />
-        <MarkdownRenderer content={markdownContent} />
+        <MarkdownRenderer content={markdownContentOverview} />
+        <MarkdownRenderer content={markdownContentWwsf} />
       </>
     )
   }
