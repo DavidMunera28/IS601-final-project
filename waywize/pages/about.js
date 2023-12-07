90e67b799980dd46b69ada1e606b0fdb605d8b59
amm0116 <amm-personal@outlook.com>
2023-12-07 15:27:00 -0500

    Import overview-text.md into about page

--- waywize/pages/about.js
+++ waywize/pages/about.js
@@ -1,10 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import MarkdownRenderer from '../components/MarkdownRenderer';
 import { Heading, Text } from "@chakra-ui/react";
 
-export default function About() {
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), '/editable-content/overview-text.md');
+  const markdownContent = fs.readFileSync(filePath, 'utf-8');
+
+  return {
+    props: {
+      markdownContent,
+    },
+  };
+}
+
+export default function About({ markdownContent }) {
     return (
       <>
         <Heading>About Page</Heading>
-        <Text>A erat nam at lectus urna. Praesent elementum facilisis leo vel fringilla. Commodo viverra maecenas accumsan lacus. Morbi tristique senectus et netus et malesuada fames ac turpis. Mauris vitae ultricies leo integer malesuada nunc vel risus. Purus sit amet volutpat consequat mauris nunc congue nisi. Pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus sit. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Pharetra pharetra massa massa ultricies mi. Lobortis scelerisque fermentum dui faucibus in ornare. Congue mauris rhoncus aenean vel elit scelerisque.</Text>
+        <MarkdownRenderer content={markdownContent} />
       </>
     )
   }
