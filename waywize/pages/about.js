3570fc24dfe79a7f8277c58084dcbe19a89f848d
amm0116 <amm-personal@outlook.com>
2023-12-07 15:42:56 -0500

    Add clarifying comments to about.js

--- waywize/pages/about.js
+++ waywize/pages/about.js
@@ -5,8 +5,12 @@ import { Heading } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 
 export async function getStaticProps() {
+
+  // For storing Overview Markdown contents
   const filePathOverview = path.join(process.cwd(), '/editable-content/overview-text.md');
   const markdownContentOverview = fs.readFileSync(filePathOverview, 'utf-8');
+
+  // For storing What We Stand For Markdown contents
   const filePathWwsf = path.join(process.cwd(), '/editable-content/what-we-stand-for-text.md');
   const markdownContentWwsf = fs.readFileSync(filePathWwsf, 'utf-8');
 
