0d0b80c7501da1f456c1514dd72cae43b26e25cc
amm0116 <amm-personal@outlook.com>
2023-12-07 15:30:17 -0500

    Add overview image placeholder to about page

--- waywize/pages/about.js
+++ waywize/pages/about.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import MarkdownRenderer from '../components/MarkdownRenderer';
 import { Heading } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), '/editable-content/overview-text.md');
@@ -18,6 +19,7 @@ export default function About({ markdownContent }) {
     return (
       <>
         <Heading as="h1">About Page</Heading>
+        <Image src="/images/OVERVIEW.png" />
         <MarkdownRenderer content={markdownContent} />
       </>
     )
