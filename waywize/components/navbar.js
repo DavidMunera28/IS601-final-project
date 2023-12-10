c1496bbc497832ed201b105a64bc6af9c2d2835a
amm0116 <amm-personal@outlook.com>
2023-12-10 17:22:49 -0500

    Add WayWize logo to navbar

--- waywize/components/navbar.js
+++ waywize/components/navbar.js
@@ -1,9 +1,10 @@
-import { List, ListItem } from "@chakra-ui/react";
+import { List, ListItem, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Navbar() {
     return (
       <nav>
+        <Text className="logo">WayWize</Text>
         <List>
             <ListItem><Link href="/">Home</Link></ListItem>
             <ListItem><Link href="about">About</Link></ListItem>
