6b9f5f7aa1b98be71f0290a9582d1ae7e7de49ab
amm0116 <amm-personal@outlook.com>
2023-12-10 12:48:49 -0500

    Add divs for quote cards

--- waywize/pages/index.js
+++ waywize/pages/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import MarkdownRenderer from '../components/MarkdownRenderer';
 import Subscribe from "@/components/subscribe";
-import { Image, Button, Heading } from "@chakra-ui/react";
+import { Image, Button, Heading, Card } from "@chakra-ui/react";
 import Link from 'next/link';
 
 export async function getStaticProps() {
@@ -28,6 +28,11 @@ export default function Home({ markdownContent }) {
 
       <div className="testimonials">
         <Heading as="h2">Read What Others Have To Say</Heading>
+        <div className="quotes">
+          <Card className="quote">1</Card>
+          <Card className="quote">2</Card>
+          <Card className="quote">3</Card>
+        </div>
       </div>
     </>
   )
