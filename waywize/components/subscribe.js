0c51fba9e5fd721e618333f80907c830873aef4f
amm0116 <amm-personal@outlook.com>
2023-12-10 14:01:27 -0500

    Remove uneeded CSS provided by Mailchimp

--- waywize/components/subscribe.js
+++ waywize/components/subscribe.js
@@ -5,17 +5,6 @@ import Link from "next/link"
 export default function Subscribe() {
     
     const htmlString = `<div id="mc_embed_shell">
-    <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-      #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
-      /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-         We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style><style type="text/css">
-      #mc-embedded-subscribe-form input[type=checkbox]{display: inline; width: auto;margin-right: 10px;}
-#mergeRow-gdpr {margin-top: 20px;}
-#mergeRow-gdpr fieldset label {font-weight: normal;}
-#mc-embedded-subscribe-form .mc_fieldset{border:none;min-height: 0px;padding-bottom:0px;}
-</style>
 <div id="mc_embed_signup">
   <form action="https://njit.us17.list-manage.com/subscribe/post?u=c04b8f9a229fa40f5d594ad9a&amp;id=8c80d48567&amp;v_id=4229&amp;f_id=003265e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
       <div id="mc_embed_signup_scroll">
