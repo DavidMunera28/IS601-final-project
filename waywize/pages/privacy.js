b3165ec8ec434d156a76b9a136060a35f5ef48cb
amm0116 <amm-personal@outlook.com>
2023-12-12 22:19:30 -0500

    Add sections to organize static vs. dynamic content

--- waywize/pages/privacy.js
+++ waywize/pages/privacy.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import MarkdownRenderer from '../components/MarkdownRenderer';
-import { Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), '/editable-content/privacy-policy-text.md');
@@ -18,8 +18,10 @@ export default function Privacy({ markdownContent }) {
     return (
       <>
         <Heading as="h1">WayWize Privacy Policy</Heading>
-        <Text>Privacy is a paramount concern for our mapping and wayfinding company, and we are committed to implementing robust measures to safeguard your personal information. Here are several key ways we prioritize and protect your privacy:</Text>
+        <Text className="privacy-static">Privacy is a paramount concern for our mapping and wayfinding company, and we are committed to implementing robust measures to safeguard your personal information. Here are several key ways we prioritize and protect your privacy:</Text>
+        <Box className="privacy-dynamic">
         <MarkdownRenderer content={markdownContent} />
+        </Box>
       </>
     )
   }
