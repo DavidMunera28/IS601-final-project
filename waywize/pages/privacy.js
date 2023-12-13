0ab6ddb528352c4e557c362b4af6ba2e32e9f1b6
amm0116 <amm-personal@outlook.com>
2023-12-12 22:48:30 -0500

    Add card element

--- waywize/pages/privacy.js
+++ waywize/pages/privacy.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import MarkdownRenderer from '../components/MarkdownRenderer';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Card, Box, Heading, Text } from '@chakra-ui/react';
 
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), '/editable-content/privacy-policy-text.md');
@@ -16,12 +16,12 @@ export async function getStaticProps() {
 
 export default function Privacy({ markdownContent }) {
     return (
-      <>
+      <Card className="privacy-policy">
         <Heading as="h1">WayWize Privacy Policy</Heading>
         <Text className="privacy-static">Privacy is a paramount concern for our mapping and wayfinding company, and we are committed to implementing robust measures to safeguard your personal information. Here are several key ways we prioritize and protect your privacy:</Text>
         <Box className="privacy-dynamic">
         <MarkdownRenderer content={markdownContent} />
         </Box>
-      </>
+      </Card>
     )
   }
