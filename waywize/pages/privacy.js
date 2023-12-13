cfb160dd0fdc49fa5d1c370e45f1651fe3e7e754
amm0116 <amm-personal@outlook.com>
2023-12-12 22:15:37 -0500

    Add static heading and text

--- waywize/pages/privacy.js
+++ waywize/pages/privacy.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import MarkdownRenderer from '../components/MarkdownRenderer';
+import { Heading, Text } from '@chakra-ui/react';
 
 export async function getStaticProps() {
   const filePath = path.join(process.cwd(), '/editable-content/privacy-policy-text.md');
@@ -16,6 +17,8 @@ export async function getStaticProps() {
 export default function Privacy({ markdownContent }) {
     return (
       <>
+        <Heading as="h1">WayWize Privacy Policy</Heading>
+        <Text>Privacy is a paramount concern for our mapping and wayfinding company, and we are committed to implementing robust measures to safeguard your personal information. Here are several key ways we prioritize and protect your privacy:</Text>
         <MarkdownRenderer content={markdownContent} />
       </>
     )
