a005142f9abc4c72ce0458234d431c987a0d7106
amm0116 <amm-personal@outlook.com>
2023-12-14 11:50:35 -0500

    Add test to check for Google tag on privacy policy page

--- waywize/tests/ga.spec.js
+++ waywize/tests/ga.spec.js
@@ -4,6 +4,7 @@ const { test, expect } = require('@playwright/test');
 
 const indexURL = 'http://localhost:3000';
 const aboutURL = 'http://localhost:3000';
+const privacyURL = 'http://localhost:3000/privacy';
 
 // Tests
 
@@ -17,6 +18,14 @@ test('Check for Google Analytics Tag on Landing (Index) Page', async ({ page }) 
 
 test('Check for Google Analytics Tag on About Page', async ({ page }) => {
     await page.goto(aboutURL);
+    await page.waitForTimeout(10000);
+    const pageContent = await page.content();
+    const hasGoogleAnalyticsTag = pageContent.includes('https://www.googletagmanager.com');
+    expect(hasGoogleAnalyticsTag).toBe(true);
+  });
+
+  test('Check for Google Analytics Tag on Privacy Policy Page', async ({ page }) => {
+    await page.goto(privacyURL);
     await page.waitForTimeout(10000);
     const pageContent = await page.content();
     const hasGoogleAnalyticsTag = pageContent.includes('https://www.googletagmanager.com');
