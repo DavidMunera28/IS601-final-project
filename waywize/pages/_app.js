4cb7efc9d714e78722f09eea23e310e561659c1b
amm0116 <amm-personal@outlook.com>
2023-12-14 19:43:07 -0500

    Add comments to root app

--- waywize/pages/_app.js
+++ waywize/pages/_app.js
@@ -10,21 +10,31 @@ export default function App({ Component, pageProps }) {
   return (
     <>
     <Head>
+      {/* Begin metadata */}
       <title>WayWize Mapping & Wayfinding Solutions</title>
       <meta name="description" content="Discover seamless outdoor and indoor navigation with our mapping and wayfinding solutions. Our software offers effortless navigational precision and a user-friendly layout." />
       <meta name="keywords" content="customized mapping, customized wayfinding, navigation app, indoor navigation, AI mapping, event venue mapping, indoor mapping software, custom business mapping" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {/* End metadata */}
+      {/* Begin favicon */}
       <link rel="icon" href="/favicon.ico" />
+      {/* End favicon */}
+      {/* Begin font download */}
       <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet"
-/>
+      />
+      {/* End font download */}
     </Head>
+    {/* Begin Google Analytics tag */}
     <GoogleAnalytics GA_MEASUREMENT_ID = 'G-F248M41PQ7' />
+    {/* End Google Analytics tag */}
+    {/* Begin visible site elements */}
     <ChakraProvider theme={theme}>
     <Navbar />
     <Component {...pageProps} />
     <Footer />
     <CookieBanner />
     </ChakraProvider>
+    {/* End visible site elements */}
     </>
   )
 }
